e850d947f911b4d0668b3aa625c637be53b6e8ae
hpark <hpark@gmail.com>
2022-07-02 13:25:33 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://user_01:RZBBELkCu8GyT5pF@cluster0.nosaa.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World~~~!");
+});
+
+app.post("/register", (req, res) => {
+  // client에서 가져온 회원가입 정보들을 데이터 베이스에 넣기
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
